58bac51eb34b75c8d7040428dabe2017b87e05b0
ncrown1190 <ncrown88@outlook.com>
2025-02-17 20:03:35 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './mode/menuItem.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'chirpus-pizza';
+  menuItems: MenuItem[] = [
+    {
+      name: 'Margherita Pizza',
+      category: 'Dinner',
+      price: 12,
+      vegetarian: true,
+      soldOut: false,
+    },
+    {
+      name: 'Caesar Salad',
+      category: 'Salad',
+      price: 8,
+      vegetarian: false,
+      soldOut: true,
+    },
+    {
+      name: 'Vegetable Pasta',
+      category: 'Dinner',
+      price: 10,
+      vegetarian: true,
+      soldOut: false,
+    },
+    {
+      name: 'Grilled Chicken',
+      category: 'Dinner',
+      price: 15,
+      vegetarian: false,
+      soldOut: false,
+    },
+    {
+      name: 'Mushroom Soup',
+      category: 'Sides',
+      price: 7,
+      vegetarian: true,
+      soldOut: true,
+    },
+    {
+      name: 'Chicken Fingers',
+      category: 'Dinner',
+      price: 11,
+      vegetarian: true,
+      soldOut: false,
+    },
+  ];
+
+  total: number = 0;
+
+  addToOrder(price: number): void {
+    this.total += price;
+  }
 }
